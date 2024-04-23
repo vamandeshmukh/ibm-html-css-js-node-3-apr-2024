e17d9ccce6fea408d8833e5fe94d669585e7e90e
Vaman Deshmukh <vamandeshmukh@gmail.com>
2024-04-23 14:47:14 +0530

    html-css-assgnmnt

--- js/03-js-function-args.js
+++ js/03-js-function-args.js
@@ -1,7 +1,48 @@
 // 03-js-function-args.js
 
-const myFun = () => {
+// const myFun = (arg) => {
+//     let output = arg - 2;
+//     console.log(output);
+// };
+
+// myFun('abc');
+
+
+// const myFun = (arg) => {
+//     let output = arg.city;
+//     console.log(output);
+// };
+
+// myFun({abc: 'Bengaluru'});
+
+
+// const myFun = (arg) => {
+//     console.log('myFun invoked.');
+//     let output = arg + 2;
+//     console.log(output);
+// };
+
+// myFun(10);
+
+// let num = 12;
+// myFun(num);
+
+
+// main funciton 
+const myFun = (arg) => {
     console.log('myFun invoked.');
+    arg(); // function call 
 };
 
-myFun(??);
+// callback function 
+const anotherFun = () => {
+    console.log('anotherFun named invoked');
+};
+
+myFun(anotherFun);
+
+// myFun(() => { });
+
+myFun(() => {
+    console.log('another anonymous function invoked');
+ });
