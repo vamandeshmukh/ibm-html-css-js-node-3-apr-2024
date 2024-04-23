62742ce04804a2881930cbbd5048284cf035167d
Vaman Deshmukh <vamandeshmukh@gmail.com>
2024-04-23 16:48:59 +0530

    html-css-assgnmnt

--- js/09-js-assignments.js
+++ js/09-js-assignments.js
@@ -1,3 +1,12 @@
 // 09-js-assignments.js
 
 console.log('09-js-assignments.js');
+
+
+// array exercieses 
+
+// 1. Create a function that takes an array of objects representing employees 
+// and returns the average / max / min salaries of all the employees.
+// 2. Write a function that takes an array of objects 
+// and sorts them based on a specified property (e.g., salary or firstName)
+
