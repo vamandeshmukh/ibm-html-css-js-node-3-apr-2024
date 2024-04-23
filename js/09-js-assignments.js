d390eccac3219c9ba687ffaf961647df6bada3f8
Vaman Deshmukh <vamandeshmukh@gmail.com>
2024-04-23 16:55:35 +0530

    html-css-assgnmnt

--- js/09-js-assignments.js
+++ js/09-js-assignments.js
@@ -3,10 +3,23 @@
 console.log('09-js-assignments.js');
 
 
-// array exercieses 
+// array exercieses
 
-// 1. Create a function that takes an array of objects representing employees 
+// 1. Create a function that takes an array of objects containing employees
 // and returns the average / max / min salaries of all the employees.
-// 2. Write a function that takes an array of objects 
+
+// 2. Write a function that takes an array of objects
 // and sorts them based on a specified property (e.g., salary or firstName)
+
+// 3. Write a function that takes a string and converts it to a number.
+// Handle cases where the string cannot be converted to a number
+// and return an error message.
+
+// 4. Write a function that takes a string and
+// and returns the number of vowels it contains.
+
+// 5. Create a function that takes a string and
+// reverses the order of words (not characters) in it.
+
+// 6. Demonstrate your understanding of rest parameters and spread operators 
 
