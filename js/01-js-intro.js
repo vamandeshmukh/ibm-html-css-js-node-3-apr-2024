2494f70d7cda47de212d82f3dccde3ec2191ece1
Vaman Deshmukh <vamandeshmukh@gmail.com>
2024-04-23 12:27:12 +0530

    html-css-assgnmnt

---
+++ js/01-js-intro.js
@@ -0,0 +1,70 @@
+
+
+console.log('JavaScript intro');
+
+// keywords, identifiers, literals, operators, separators,
+// variables declarations, datatypes, objects, arrays,
+// function definitions, classes and objects,
+
+// variables,
+
+
+// java - strongly typed language
+// javascript - dynamically typed language 
+
+
+// Java - 
+// int num;
+// Employee emp = new Employee();
+
+// JavaScript 
+// let num = 10;
+// console.log(num);
+// num = 'abc';
+// console.log(num);
+
+
+// num1 = 10; // don't use 
+// var num2 = 20; // don't use 
+// const num3 = 30; // default choice 
+// let num4 = 40; // when variable variables to be created 
+
+
+// const calcGst = () => {
+
+// };
+
+// class MyClass {
+
+// }
+
+console.log('Datatypes in JS');
+
+// const eid = 101;
+// const firstName = 'Sonu';
+// const salary = 10.5;
+// const isMarried = false;
+
+// console.log(eid);
+// console.log(firstName);
+// console.log(salary);
+// console.log(isMarried);
+
+// let myValue = 10;
+// console.log(myValue);
+// console.log(typeof (myValue));
+// myValue = 20.25;
+// console.log(myValue);
+// console.log(typeof (myValue)); 
+// myValue = 'abc';
+// console.log(myValue);
+// console.log(typeof (myValue)); 
+// myValue = false;
+// console.log(myValue);
+// console.log(typeof (myValue)); 
+
+
+const num2 = 20;
+console.log(num2);
+num2 = 'abc';
+console.log(num2);
---
+++ js/script.js
@@ -0,0 +1,14 @@
+ 
+console.log('Introduction to JavaScript');
+console.log("Introduction to JavaScript");
+console.log(`Introduction to JavaScript`);
+
+let firstName = 'Sonu';
+let message = 'Hi ' + firstName + '! How are you?';
+let message2 = `Hi ${firstName}! How are you?`;
+
+console.log(message);
+console.log(message2);
+
+
+
