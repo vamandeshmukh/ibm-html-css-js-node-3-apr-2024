7edd583fe2c5f385a44bb2d7a0dab22970491a27
Vaman Deshmukh <vamandeshmukh@gmail.com>
2024-04-23 15:02:34 +0530

    html-css-assgnmnt

--- js/03-js-function-args.js
+++ js/03-js-function-args.js
@@ -28,21 +28,45 @@
 // myFun(num);
 
 
-// main funciton 
-const myFun = (arg) => {
-    console.log('myFun invoked.');
-    arg(); // function call 
-};
+// // main funciton 
+// const myFun = (arg) => {
+//     console.log('myFun invoked.');
+//     arg(); // function call 
+// };
 
-// callback function 
-const anotherFun = () => {
-    console.log('anotherFun named invoked');
-};
+// // callback function 
+// const anotherFun = () => {
+//     console.log('anotherFun named invoked');
+// };
 
-myFun(anotherFun);
+// myFun(anotherFun);
 
-// myFun(() => { });
+// // myFun(() => { });
 
-myFun(() => {
-    console.log('another anonymous function invoked');
- });
+// myFun(() => {
+//     console.log('another anonymous function invoked');
+//  });
+
+
+// const myFun = (arg) => {
+//     console.log('myFun invoked.');
+//     arg({city: 'Hyderabad'}); // function call 
+// };
+
+
+// myFun((abc) => {
+//     console.log('another anonymous function invoked');
+//     console.log(abc.city);
+//  });
+
+
+setTimeout(args1, arg2);
+setTimeout(() => {}, 1000);
+
+console.log('one');
+
+setTimeout(() => {
+    console.log('two');
+}, 1000);
+
+console.log('three');
