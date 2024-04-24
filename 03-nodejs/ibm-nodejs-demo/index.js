09ab01b080ff750f9828e3eb45365dcd3dd6dea7
Vaman Deshmukh <vamandeshmukh@gmail.com>
2024-04-24 17:58:07 +0530

    nodejs

--- 03-nodejs/ibm-nodejs-demo/index.js
+++ 03-nodejs/ibm-nodejs-demo/index.js
@@ -1,23 +1,62 @@
 
-console.log('node project working...');
+import express from "express";
+import axios from "axios";
 
+const app = express();
+const port = 3000;
 const url = 'https://jsonplaceholder.typicode.com/users/2';
 
-import axios from 'axios';
+// app.listen( port, () => {});
 
-axios.get(url)
-    .then(res => console.log(res))
-    .catch(err => console.log(err));
+app.listen(port, () => {
+    console.log('app started.');
+});
 
-// ReferenceError: axios is not defined 
+app.get('/', (request, response) => {
+    axios.get(url)
+        .then((res) => {
+            console.log(res);
+            response.send(res.username);
+        })
+        .catch(err => console.log(err));
+});
 
-/*
-// Comparison 
-// Spring+Maven  -  Node+NPM
-// pom.xml - package.json  
-// maven update - npm install 
-// run project - npm start 
-// project - package 
-// mvnrepository.com - npmjs.com 
-// 
-*/
+app.get('/contact', (request, response) => {
+    console.log('contact');
+    response.send('Please contact us.');
+});
+
+app.get('/about', (request, response) => {
+    console.log('about');
+    response.send('About our company...');
+});
+
+
+
+
+
+
+
+
+// console.log('node project working...');
+
+// const url = 'https://jsonplaceholder.typicode.com/users/2';
+
+// import axios from 'axios';
+
+// axios.get(url)
+//     .then(res => console.log(res))
+//     .catch(err => console.log(err));
+
+// // ReferenceError: axios is not defined
+
+// /*
+// // Comparison
+// // Spring+Maven  -  Node+NPM
+// // pom.xml - package.json
+// // maven update - npm install
+// // run project - npm start
+// // project - package
+// // mvnrepository.com - npmjs.com
+// //
+// */
