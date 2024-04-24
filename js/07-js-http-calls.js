36e6d258fce30041194cde91acaf41dff3c6cb68
Vaman Deshmukh <vamandeshmukh@gmail.com>
2024-04-24 11:44:03 +0530

    html-css-assgnmnt

--- js/07-js-http-calls.js
+++ js/07-js-http-calls.js
@@ -8,12 +8,14 @@ const url = 'https://jsonplaceholder.typicode.com/users/2';
 // fetch(url).then(() => {}).then(() => {}).catch(() => {});
 
 
-fetch(url)
-    .then((res) => {
-        console.log(res);
-        return res.json();
-    }).then((resp) => {
-        console.log(resp);
-    })
-    .catch((error) => { console.log(error); });
+// fetch(url)
+//     .then((res) => {
+//         console.log(res);
+//         return res.json();
+//     }).then((resp) => {
+//         console.log(resp);
+//     })
+//     .catch((error) => { console.log(error); });
 
+
+fetch(url).then(res => res.json()).then(resp => console.log(resp)).catch(err => console.log(err));
