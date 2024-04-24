ad0a5a4974b141778714b8bbaa0d4111a05ec811
Vaman Deshmukh <vamandeshmukh@gmail.com>
2024-04-24 10:30:48 +0530

    async

--- js/06-js-async.js
+++ js/06-js-async.js
@@ -1,26 +1,65 @@
 // node 06-js-async.js
 
-const getMessage0 = () => {
-    console.log('getMessage function called.');
-    setTimeout(() => {
-        return { message: 'Have fun!' };
-    }, 2000);
-};
+// // problems of asynchronous JavaScipt  
+// -------------------------------------
 
-const getMessage = (arg) => {
-    console.log('getMessage function called.');
-    setTimeout(() => {
-        arg({ message: 'Have fun!' })
-    }, 2000);
-};
-
-getMessage((abc) => {
-    console.log(abc.message);
-});
+// const getMessage = () => {
+//     console.log('getMessage function called.');
+//     setTimeout(() => {
+//         return { message: 'Have fun!' };
+//     }, 2000);
+// };
 
 // const output = getMessage();
 // console.log(output.message);
 
+// solution 1 - callback 
+// ---------------------
 
+// const getMessage = (arg) => {
+//     console.log('getMessage function called.');
+//     setTimeout(() => {
+//         arg({ message: 'Have fun!' })
+//     }, 2000);
+// };
+
+// getMessage((abc) => {
+//         console.log(abc.message);
+//     });
+
+// solution 2 - Promise 
+// ---------------------
+
+// const getMessage = () => {
+//     return new Promise();
+// };
+
+// const getMessage = () => {
+//     return new Promise(() => { });
+// };
+
+// const getMessage = () => {
+//     return new Promise((resolve, reject) => { });
+// };
+
+const getMessage = () => {
+    return new Promise((resolve, reject) => {
+        const isDataAvailable = false; // true, false 
+        setTimeout(() => {
+            if (isDataAvailable)
+                resolve({ message: 'Have fun!' });
+            else
+                reject({ message: 'Data not available!' })
+        }, 2000);
+    });
+};
+
+// getMessage()
+//     .then(() => {}) // success 
+//     .catch(() => {}); // error 
+
+getMessage()
+    .then((response) => { console.log(response.message); })
+    .catch((error) => { console.log(error); });
 
 
