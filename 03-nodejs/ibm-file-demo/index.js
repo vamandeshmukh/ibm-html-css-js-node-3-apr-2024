aea3250527c761383d2612e614fdbeba387537bd
Vaman Deshmukh <vamandeshmukh@gmail.com>
2024-04-25 12:56:08 +0530

    nodejs

--- 03-nodejs/ibm-file-demo/index.js
+++ 03-nodejs/ibm-file-demo/index.js
@@ -1,18 +1,31 @@
 import fs from 'fs';
 
+const dataFile = 'emp.json';
 const dataFile2 = 'emp2.json';
+
+// fs.readFile(filepath, 'utf8', (err, data) => { })
+// fs.writeFile(filepath, 'utf8', (err) => { })
+
+fs.readFile(dataFile, 'utf8', (err, data) => {
+    if (err) {
+        console.log(err);
+    }
+    else {
+        console.log(JSON.parse(data));
+    }
+});
 
 const contentToWrite = {
     "eid": 102,
     "firstName": "Monu",
     "salary": 20.25
-}
+};
 
-fs.writeFile(dataFile2, JSON.stringify(contentToWrite), 'utf8', (err) => {
+fs.writeFile(dataFile2, JSON.stringify(contentToWrite), 'utf8', (err, data) => {
     if (err)
         console.log(err);
     else
-        console.log(`File written successfully!`);
+        console.log(`${data} written successfully!`);
 
 });
 
