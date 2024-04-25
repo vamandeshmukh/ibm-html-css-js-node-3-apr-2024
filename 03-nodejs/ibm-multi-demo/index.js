e0c15e9b76b8c4523a94d39751e1517cc130cb88
Vaman Deshmukh <vamandeshmukh@gmail.com>
2024-04-25 15:36:01 +0530

    nodejs

--- 03-nodejs/ibm-multi-demo/index.js
+++ 03-nodejs/ibm-multi-demo/index.js
@@ -1,6 +1,18 @@
-import {addNums } from './math.js';
 
-let sum = addNums(10, 20);
+// import {addNums, subNums } from './math.js';
+// import {addNums, subNums } from './math.js';
+// import { addNums } from './math.js';
 
-console.log(sum);
+// let sum = addNums(10, 20);
+// console.log(sum);
 
+// let balance = subNums(30, 20);
+// console.log(balance);
+
+// let sum = addNums(10, 20);
+// console.log(sum);
+
+// import {addNums, subNums } from './math.js';
+
+// let balance = subNums(30, 20);
+// console.log(balance);
--- 03-nodejs/ibm-multi-demo/math.js
+++ 03-nodejs/ibm-multi-demo/math.js
@@ -1,5 +1,15 @@
 
-export const addNums = (a, b) => a + b;
+// export const addNums = (a, b) => a + b;
+// export const subNums = (a, b) => a - b;
 
-export const subNums = (a, b) => a - b;
+// const addNums = (a, b) => a + b;
+// const subNums = (a, b) => a - b;
 
+// export { addNums, subNums };
+
+// export const addNums = (a, b) => a + b;
+
+export const math = {
+    addNums: (a, b) => a + b,
+    subNums: (a, b) => a - b
+};
