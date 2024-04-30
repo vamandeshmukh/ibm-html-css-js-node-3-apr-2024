4b14bffc6ff5020eb658c6764629b382a7d59974
Vaman Deshmukh <vamandeshmukh@gmail.com>
2024-04-30 12:11:02 +0530

    sendemail

--- 03-nodejs/ibm-emp-demo/app-tests.js
+++ 03-nodejs/ibm-emp-demo/app-tests.js
@@ -1,3 +1,33 @@
+import supertest from "supertest";
+import {app} from './index.js';
 
-// test cases 
+describe('it should pass', () => {
 
+    it('should pass', () => {
+        expect(5 + 5).toBe(10);
+    });
+
+    it('should succeed login', () => {
+
+    })
+
+});
+
+
+// describe();
+// it();
+
+
+// describe(arg1, arg2);
+// describe('it should pass', () => { });
+
+// it(a1, a2);
+// it('test should pass', () => { });
+
+// describe('it should pass', () => {
+
+//     it();
+//     it();
+//     it();
+
+// });
