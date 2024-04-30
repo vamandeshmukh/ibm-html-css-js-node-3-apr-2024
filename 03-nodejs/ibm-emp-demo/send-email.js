7bf50520597b1a2ac73427e366ef864661d48073
Vaman Deshmukh <vamandeshmukh@gmail.com>
2024-04-30 10:41:57 +0530

    sendemail

--- 03-nodejs/ibm-emp-demo/send-email.js
+++ 03-nodejs/ibm-emp-demo/send-email.js
@@ -42,27 +42,21 @@ const transporter = nodemailer.createTransport({
 
 // code with dynamic values and proper response   
 const sendEmail = (mailProps) => {
-
     const mailOptions = {
         from: 'asdf@yahoo.com',
         to: mailProps.to,
         subject: mailProps.subject,
         text: mailProps.text
     };
-    console.log(mailOptions);
-
     return new Promise((resolve, reject) => {
         try {
-            const success = transporter.sendMail(mailOptions);
-            console.log(success);
+            const success =
+                transporter.sendMail(mailOptions);
             resolve(success);
         }
         catch (error) {
-            console.log(error);
             reject(error);
         }
     });
 };
-
-
 export { sendEmail };
