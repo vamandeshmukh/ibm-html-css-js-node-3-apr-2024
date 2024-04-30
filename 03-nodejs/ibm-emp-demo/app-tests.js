10b7336371ede8dfcd9f43bdc66b8e9372b572b2
Vaman Deshmukh <vamandeshmukh@gmail.com>
2024-04-30 12:45:17 +0530

    sendemail

--- 03-nodejs/ibm-emp-demo/app-tests.js
+++ 03-nodejs/ibm-emp-demo/app-tests.js
@@ -1,17 +1,56 @@
 import supertest from "supertest";
-import {app} from './index.js';
+import { app } from './index.js';
 
-describe('it should pass', () => {
+const request = supertest(app);
+let token = '';
 
-    it('should pass', () => {
-        expect(5 + 5).toBe(10);
+// beforeAll(); // login here and save the token 
+
+describe('test setup', () => {
+    beforeAll(async () => {
+        const res = await request
+            .post('/login')
+            .send({ username: 'sonu', password: 'sonu' });
+        token = res.body.token;
+    });
+});
+
+
+describe('it should pass tests - ', () => {
+
+    it('should find the number of employees', async () => {
+        const res = await request
+            .get('/employees');
+        console.log(res);
+        expect(res.status).toBe(200);
+        expect(res.body.size).toBe(45);
     });
 
-    it('should succeed login', () => {
-
-    })
-
 });
+
+
+
+// describe('it should pass tests - ', () => {
+
+//     it('should succeed login', async () => {
+//         const res = await request
+//             .post('/login')
+//             .send({ username: 'sonu', password: 'sonu' });
+//         expect(res.status).toBe(200);
+//         expect(res.body.message).toBe('Login successful');
+//         expect(res.body.token).toBeTruthy();
+//     });
+
+//     it('should fail login', async () => {
+//         const res = await request
+//             .post('/login')
+//             .send({ username: 'asdf', password: 'asdf' });
+//         expect(res.status).not.toBe(200);
+//         expect(res.body.message).toBe('Invalid credentials');
+
+//     })
+
+// });
 
 
 // describe();
@@ -31,3 +70,61 @@ describe('it should pass', () => {
 //     it();
 
 // });
+
+
+// import supertest from "supertest";
+
+// import { app } from './index.js';
+
+// const request = supertest(app);
+
+// // beforeAll(); // login here and save the token
+
+// // beforeEach();
+// // afterAll();
+// // afterEach();
+
+// describe('test setup', () => {
+//     beforeAll();
+// });
+
+// describe('it should pass tests - ', () => {
+
+//     it('should succeed login', async () => {
+//         const res = await request
+//             .post('/login')
+//             .send({ username: 'sonu', password: 'sonu' });
+//         expect(res.status).toBe(200);
+//         expect(res.body.message).toBe('Login successful');
+//         expect(res.body.token).toBeTruthy();
+//     });
+
+//     it('should fail login', async () => {
+//         const res = await request
+//             .post('/login')
+//             .send({ username: 'asdf', password: 'asdf' });
+//         expect(res.status).not.toBe(200);
+//         expect(res.body.message).toBe('Invalid credentials');
+
+//     })
+
+// });
+
+
+// // describe();
+// // it();
+
+
+// // describe(arg1, arg2);
+// // describe('it should pass', () => { });
+
+// // it(a1, a2);
+// // it('test should pass', () => { });
+
+// // describe('it should pass', () => {
+
+// //     it();
+// //     it();
+// //     it();
+
+// // });
